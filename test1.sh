da6bbf33e2b8b0a72a410d37b8dd7efb4d938170
ashifa22 <ashifayusafali@gmail.com>
2019-02-21 14:40:09 +0530

    intial commit

---
+++ test1.sh
@@ -0,0 +1,7 @@
+#!\bin\sh
+echo "your username: $USER"
+echo "my current shell is $SHELL($0)"
+echo "current working directory:$PWD"
+echo "operating system type :$OSTYPE"
+echo "Current working directory: `pwd`"
+who --count
---
+++ test2.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo 1.your os and version,release number,kernal version:
+cat /etc/*release
+echo 2. all available shells
+cat /etc/shells
+echo 3. computer cpu information like processor type,speed etc.
+cat /proc/meminfo
+echo 6. file system
+df
+
+
+
+
+
